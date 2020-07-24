35f45a35a702ba16d4dac2f78ac3516031be1726
Gabriel Seals <sealsgj@gmail.com>
2020-07-24 16:30:53 -0500

    Updates

---
+++ precache-manifest.e833516541e21b5d4d1effe43044d216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02eafe11051b62ad2716c30fe40dee3",
+    "url": "/react-currency-conversion/index.html"
+  },
+  {
+    "revision": "14aec6762f06a6ddb6e4",
+    "url": "/react-currency-conversion/static/css/main.33d7be06.chunk.css"
+  },
+  {
+    "revision": "4321472284bec383f728",
+    "url": "/react-currency-conversion/static/js/2.238c1476.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-currency-conversion/static/js/2.238c1476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14aec6762f06a6ddb6e4",
+    "url": "/react-currency-conversion/static/js/main.ea6b5bbf.chunk.js"
+  },
+  {
+    "revision": "1dda03d49e335427049c",
+    "url": "/react-currency-conversion/static/js/runtime-main.46b0b0f0.js"
+  }
+]);
